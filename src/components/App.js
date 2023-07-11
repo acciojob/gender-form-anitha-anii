27681d3dee80b0ad85ca58e5e2ac30dd457f666c
anitha-anii <anithachowdary355@gmail.com>
2023-07-11 11:38:21 +0530

    'changes'

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
+function App(){
+ const [selected,setSelected]=useState('');
+
+return(
+  <div>
+    <h2>Select your gender</h2>
+    <input type='radio' name='gender' value='Male' checked={selected === 'Male'}  onChange={(e) => setSelected(e.target.value)}/>
+    <label>Male</label>
+    <input type='radio' name='gender'value='Female' checked={selected === 'Female'}  onChange={(e) => setSelected(e.target.value)}/>
+    <label>Female</label>
+  {selected === 'Male' && (<div>
+    <h2>Select your shirt size</h2>
+    <select>
+    <option>Select size</option>
+      <option>Small</option>
+      <option>Medium</option>
+      <option>Large</option>
+    </select>
     </div>
-  )
+)}
+{selected === 'Female' && (
+    <div>
+    <h2>Select your dress size</h2>
+    <select>
+      <option>Select size</option>
+      <option>2</option>
+      <option>4</option>
+      <option>6</option>
+    </select>
+    </div>
+)}
+  </div>
+
+) 
+
 }
 
 export default App
